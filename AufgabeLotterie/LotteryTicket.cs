9bda7355ff546084692e99c530388f8325d0bc61
mathias angerer <mangero@mathiass-MacBook-Pro.local>
2020-06-04 11:10:47 +0200

    first commit

---
+++ AufgabeLotterie/LotteryTicket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AufgabeLotterie
+{
+    public class LotteryTicket
+    {
+        public string Ticket;
+        public LotteryTicket(string RndTicket)
+        {
+            Ticket = RndTicket;
+        }
+
+        public bool CheckTicket(string UserNumbers)
+        {
+            if (UserNumbers.Equals(Ticket))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string ReturnTicket()
+        {
+            return "You won!, Ticket number: " + Ticket;
+        }
+
+        public string Digits(string UsersNumbers)
+        {
+            int Digits = 0;
+
+            for(int i = 0; i < UsersNumbers.Length; i++)
+            {
+                if (UsersNumbers[i].Equals(Ticket[i]))
+                {
+                    Digits++;
+                }
+            }
+            return "You got " + Digits + " right!";
+        }
+          
+    }
+}
---
+++ AufgabeLotterie/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AufgabeLotterie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Match;
+            Random Random = new Random();
+            string RndTicket = Convert.ToString(Random.Next(100000, 1000000));
+            Console.WriteLine(RndTicket);
+            LotteryTicket RandomTicket = new LotteryTicket(RndTicket);
+            do
+            {
+                Console.WriteLine("6 stelliges Ticket oder exit eingeben");
+                string Choice = Console.ReadLine();
+                if (Choice != "exit" && Choice.Length == 6)
+                {
+                    Match = RandomTicket.CheckTicket(Choice);
+                    if (Match)
+                    {
+                        Console.WriteLine(RandomTicket.ReturnTicket());
+                        
+                    } else
+                    {
+                        Console.WriteLine(RandomTicket.Digits(Choice));
+                    }
+
+                } else
+                {
+                    Match = true;
+                }
+            } while (!Match);
+            Console.WriteLine("Lottery exit");
+        }
+    }
+}
